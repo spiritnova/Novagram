0171d256bde01425b935a366f14da0ff0fc15651
Ibrahim Abboud <coraynova@hotmail.com>
2023-01-16 22:29:51 +0200

    Finished the Create post button design

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -8,20 +8,35 @@ import { faComment } from '@fortawesome/free-regular-svg-icons'
 import { faHeart } from '@fortawesome/free-regular-svg-icons'
 import { faSquarePlus } from '@fortawesome/free-regular-svg-icons'
 import { faUser } from '@fortawesome/free-regular-svg-icons'
+import { faPhotoFilm } from '@fortawesome/free-solid-svg-icons'
 
 import { Link, useMatch, useResolvedPath } from "react-router-dom"
+import { useState } from 'react'
 import { useTheme } from '../ThemeContext'
 
 import Dropdown from './Dropdown'
 import AuthContext from '../context/auth-context'
+import Backdrop from './UI Kit/Backdrop'
 
 export default function Sidebar(props){
 
+    const [showBackdrop, setShowBackdrop] = useState(false)
+    const [showModal, setShowModal] = useState(false)
     const darkTheme = useTheme()
 
     darkTheme // Used to match the Body and Wrapper'S color depending on the theme
     ? document.body.style = 'background : #121212'
     : document.body.style = 'background : #f2f2f2'
+
+    const handleShow = () => {
+        setShowBackdrop(true)
+        setShowModal(true)
+    }
+
+    const handleClose = () => {
+        setShowBackdrop(false)
+        setShowModal(false)
+    }
 
     return (
         <AuthContext.Consumer>
@@ -52,7 +67,7 @@ export default function Sidebar(props){
                                     </button>
                                 </li>
                                 <li>
-                                    <button className={darkTheme ? styles.buttons : styles['buttons-light']}>
+                                    <button className={darkTheme ? styles.buttons : styles['buttons-light']} onClick={handleShow}>
                                     <FontAwesomeIcon icon={faSquarePlus} className={darkTheme ? styles.icons : styles['icons-light']}/> <span className={styles['nav-names']}>Create</span>
                                     </button>
                                 </li>
@@ -63,6 +78,26 @@ export default function Sidebar(props){
         
                             <Dropdown onLogout={props.onLogout}/>
                         </nav> : ''}
+                       {showBackdrop && <Backdrop/>}
+                        {showModal && <div className={styles.modal}>
+                            <div>
+                                <p>Create new post</p>
+                            </div>
+
+                            <div>
+                                <FontAwesomeIcon icon={faPhotoFilm} className={styles['modal-icon']}></FontAwesomeIcon>
+                            </div>
+
+                            <p>Drag photos and videos here</p>
+
+                            <div>
+                                <button className={styles['modal-button']}>Select from computer</button>
+                            </div>
+
+                            <div>
+                                <button onClick={handleClose}>Close</button>
+                            </div>
+                       </div>}
                     </div>
                 )
             }}
