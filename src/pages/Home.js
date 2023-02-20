87464a9d7e29007de662d6870a7ebf1155455788
Ibrahim Abboud <coraynova@hotmail.com>
2023-02-20 22:36:39 +0200

    Home page finished

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,12 +2,26 @@ import { faComment, faHeart } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { useQuery } from '@tanstack/react-query'
 import axios from 'axios'
+import { useRef, useState } from 'react'
+import { Link, Outlet, useLocation } from 'react-router-dom'
 import Wrapper from '../components/UI Kit/Wrapper'
 import styles from './Home.module.css'
+import sendComment from '../api/sendComment'
+import Post from './Profile/Post'
 
 export default function Home(){
+    const [showPostButton, setShowPostButton] = useState(false)
+    const [enteredComment ,setEnteredComment] = useState('')
+    const [showModal, setShowModal] = useState(false)
+    const [postId, setPostId] = useState()
+
+    const location = useLocation();
 
     const username = sessionStorage.getItem('username')
+    const user_id = sessionStorage.getItem('user_id')
+    
+    const comment = useRef()
+    const postBtn = useRef()
 
     const homeQuery = useQuery({
         queryKey: ["home", username],
@@ -16,19 +30,38 @@ export default function Home(){
         }),
       })
 
+    const commentChangeHandler = (e) => {
+      setEnteredComment(e.target.value)
+      if(e.target.value !== '')
+      {
+        setShowPostButton(true)
+      }
+      else{
+        setShowPostButton(false)
+      }
+    }
+
     return(
         <Wrapper>
+          <Outlet/>
             <div className={styles.cards}>
               {homeQuery.data?.data.map(post => (
-                <div className={styles.card}>
+                <div key={post.id} className={styles.card}>
                   <div className={styles.info}>
                     <div className={styles.pfp}></div>
-                    <div className={styles.username}>{post.user}</div>
+                    <div className={styles.username}>
+                      <Link to={`profile/${post.user}`}>
+                        {post.user}
+                      </Link>
+                    </div>
                     <div className={styles.date}>1 week ago</div>
                   </div>
 
                   <div className={styles.img}>
-                    <img alt="posts" src={post.picture}/>
+                    <img alt="posts" src={post.picture} onClick={() => {
+                      setShowModal(true)
+                      setPostId(post.id)
+                    }}/>
                   </div>
 
                   <div className={styles.buttons}>
@@ -44,11 +77,34 @@ export default function Home(){
                     <div>245 likes</div>
                     <div>Comment</div>
                     <button>View all 42 comments</button>
-                    <input type="text" placeholder='Add a comment...'></input>
+                    <div className={styles.post}>
+                      <input 
+                      type="text" 
+                      ref={comment} 
+                      onChange={commentChangeHandler} 
+                      placeholder='Add a comment...'
+                      value={enteredComment}
+                      />
+                      {showPostButton && <button onClick={() => {
+                        sendComment({
+                          comment : comment.current.value,
+                          user_id: user_id,
+                          id : post.id
+                        })
+                        setEnteredComment('')
+                      }} ref={postBtn}>Post</button>}
+                    </div>
                   </div>
                 </div>
               ))}
             </div>
+          {showModal &&
+            <Post 
+              onClose={() => setShowModal(false)}
+              realRoute={location.pathname}
+              pseudoRoute={`/post/${postId}`}
+            />
+          }
         </Wrapper>
     )
 }
