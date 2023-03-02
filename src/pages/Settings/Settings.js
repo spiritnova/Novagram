5e85675d771c3a89a462d06f6fddf6890670f054
Ibrahim Abboud <coraynova@hotmail.com>
2023-03-02 21:43:07 +0200

    Optimized settings for mobile

--- src/pages/Settings/Settings.js
+++ src/pages/Settings/Settings.js
@@ -1,8 +1,11 @@
 import styles from './Settings.module.css'
 import { Link, Outlet } from 'react-router-dom'
 import { useTheme } from '../../context/ThemeContext'
+import { useState } from 'react'
 
 export default function Settings(){
+
+    const [showDropdown, setShowDropdown] = useState(false)
 
     const darkTheme = useTheme()
     return(
@@ -16,6 +19,18 @@ export default function Settings(){
                     <li><Link to="/settings/login_activity">Login Activity</Link></li>
                     <li><Link to="/settings/help">Help</Link></li>
                 </ul>
+            </div>
+
+            <div className={styles.mobileNavbar}>
+                <button onClick={() => setShowDropdown(prev => !prev)} className={styles.dropbtn}>More settings</button>
+                {showDropdown && <div className={styles.dropdownContent}>
+                    <Link onClick={() => setShowDropdown(false)} to="/settings">Edit profile</Link>
+                    <Link onClick={() => setShowDropdown(false)} to="/settings/password_change">Change password</Link>
+                    <Link onClick={() => setShowDropdown(false)} to="/settings/emails/notifications">Email notifications</Link>
+                    <Link onClick={() => setShowDropdown(false)} to="/settings/privacy_and_security">Privacy and Security</Link>
+                    <Link onClick={() => setShowDropdown(false)} to="/settings/login_activity">Login Activity</Link>
+                    <Link onClick={() => setShowDropdown(false)} to="/settings/help">Help</Link>
+                </div>}
             </div>
             
             <div className={styles.container}>
